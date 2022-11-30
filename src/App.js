bd7163f5140fd05eec3ce8e38e504bbc95990b5a
Leandro Martins <leandro.martins,85@gmail.com>
2022-11-30 04:34:50 -0300

    Estrutura inicial

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import {  BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Header from './components/Header'
+import Home from './components/Home'
+import Login from './components/Login/Login'
+import Footer from './components/Footer'
+
+import './App.css'
 
 function App() {
   return (
     <div >
-      DOGS
+      <BrowserRouter >
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+      </BrowserRouter>
     </div>
   );
 }
