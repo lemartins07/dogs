b97153c2a7156c7195fc732d6e837d3fb19e595c
Leandro Martins <leandro.martins,85@gmail.com>
2022-12-01 23:03:51 -0300

    userForm

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,47 +1,37 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import Input from '../../components/Form/Input'
 import Button from '../../components/Form/Button'
+import useForm from '../../hooks/useForm'
 
 const LoginForm = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  const username = useForm()
+  const password = useForm()
 
   function handleSubmit(event) {
     event.preventDefault()
-
-    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ username, password }),
-    })
-      .then((response) => {
-        console.log(response)
-        return response.json()
+    if (username.validate() && password.validate()) {
+      fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(),
       })
-      .then((json) => console.log(json))
+        .then((response) => {
+          console.log(response)
+          return response.json()
+        })
+        .then((json) => console.log(json))
+    }
   }
 
   return (
     <section>
       <h1>Login</h1>
       <form onSubmit={handleSubmit}>
-        <Input
-          label="Usuário"
-          type="text"
-          name="username"
-          value={username}
-          onChange={({ target }) => setUsername(target.value)}
-        />
-        <Input
-          label="Senha"
-          type="password"
-          name="password"
-          value={password}
-          onChange={({ target }) => setPassword(target.value)}
-        />
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
         <Button>Entrar</Button>
       </form>
     </section>
