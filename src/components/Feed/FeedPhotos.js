1bd3506ec283a08a75de4a14d1829a7e6e611fd4
Leandro Martins <leandro.martins,85@gmail.com>
2022-12-22 20:10:32 -0300

    0820 Feed

---
+++ src/components/Feed/FeedPhotos.js
@@ -0,0 +1,37 @@
+import { useEffect } from 'react'
+import useFecth from '../../hooks/useFetch'
+import { PHOTOS_GET } from '../../services/api'
+import FeedPhotoItem from './FeedPhotoItem'
+import Error from '../helper/Error'
+import Loading from '../helper/Loading'
+
+import styles from './FeedPhotos.module.css'
+
+// { setInfite, page, user, setModalPhoto}
+
+const FeedPhotos = () => {
+  const { data, loading, error, request } = useFecth()
+
+  useEffect(() => {
+    async function fetchPhotos() {
+      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 })
+      const { response, json } = await request(url, options)
+      console.log(json, response)
+    }
+    fetchPhotos()
+  }, [request])
+
+  if (error) return <Error error={error} />
+  if (loading) return <Loading />
+  if (data)
+    return (
+      <ul className={`animeLeft ${styles.feed}`}>
+        {data.map((photo) => (
+          <FeedPhotoItem key={photo.id} photo={photo} />
+        ))}
+      </ul>
+    )
+  else return null
+}
+
+export default FeedPhotos
