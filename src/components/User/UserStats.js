d037950e8af8bd6aefa0a9ee22c8cc0aa431a9d8
Leandro Martins <leandro.martins.85@gmail.com>
2023-01-07 19:39:22 -0300

    0835 Biblioteca externa 1 | 0836 Biblioteca externa 2 | 0837 Lazy e Suspense

--- src/components/User/UserStats.js
+++ src/components/User/UserStats.js
@@ -1,13 +1,33 @@
 import React from 'react'
 import Head from '../helper/Head'
+import useFetch from '../../hooks/useFetch'
+import { STATS_GET } from '../../services/api'
+import Error from '../helper/Error'
+import Loading from '../helper/Loading'
+const UserStatsGraphs = React.lazy(() => import('./UserStatsGraphs'))
 
 const UserStats = () => {
-  return (
-    <section>
-      <Head title="Estatísticas" />
-      ESTATISTICAS
-    </section>
-  )
+  const { data, error, loading, request } = useFetch()
+
+  React.useEffect(() => {
+    async function getData() {
+      const token = window.localStorage.getItem('token')
+      const { url, options } = STATS_GET(token)
+      await request(url, options)
+    }
+    getData()
+  }, [request])
+
+  if (error) return <Error error={error} />
+  if (loading) return <Loading />
+  if (data)
+    return (
+      <section>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </section>
+    )
+  else return null
 }
 
 export default UserStats
